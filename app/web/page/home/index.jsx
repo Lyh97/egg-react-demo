4a32876264578562171cde48224ae6b8ea895a9d
liuyuhang <liuyuhang@Hunter-Liu.local>
2021-06-16 22:22:16 +0800

    test1

--- app/web/page/home/index.jsx
+++ app/web/page/home/index.jsx
@@ -26,7 +26,7 @@ class HomeIndex extends React.Component {
                     <div className="logo" />
                     <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
                         <Menu.Item key="1" icon={<UserOutlined />}>
-                        nav 1
+                        nav 12
                         </Menu.Item>
                         <Menu.Item key="2" icon={<VideoCameraOutlined />}>
                         nav 2
